3ee1f3aded92cdc695be7eaf14bfd60916c5b295
David Rimui <110022594+DavidRimui@users.noreply.github.com>
2023-05-26 10:06:23 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 10, "M", "Black", "gently" ,TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 14, "M", "Brown", "gently",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 15, "F", "Brown", "gently" ,TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 5, "M", "Black", "gently",TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 13, "F", "Black", "gently" ,TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 14, "M", "Brown", "gently" ,TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 21, "F", "Black", "gently",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 2, "M", "Black", "gently" ,TRUE);
